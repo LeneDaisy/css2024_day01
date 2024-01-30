20c2ba230f977d887b579f6f75e1b416b8636e86
LeneDaisy <157574288+LeneDaisy@users.noreply.github.com>
2024-01-30 10:35:24 +0200

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 11:51:36 2024
+
+@author: lenenortje
+"""
+
+print("my first python file/script/recipe")
+
+density = 1000
+
+Density = 900
+
+mass_kg = 50
+
+#three main typs of data 
+"""
+1. int - integer - whole number
+2. float - number with decimals
+3. strings - letters, text, words, paragraphs
+"""
+
+gravity_m_2_sec = 9.8 
+
+water_level_m_above_sea_level= 14.89
+
+pi = 3.14
+
+fruit = "banana"
+
+element = "Potassium"
+
+parasite = "plasmodium"
+
+delivered_prematurely = "<6 month, >6 months, < 9 months"
